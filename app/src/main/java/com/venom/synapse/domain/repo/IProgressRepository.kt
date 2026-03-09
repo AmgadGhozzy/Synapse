03536a176e9badd5a5d0a3faadee8e4aeb5c1616
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-03-10 00:26:40 +0200

    add IProgressRepository interface

---
+++ app/src/main/java/com/venom/synapse/domain/repo/IProgressRepository.kt
@@ -0,0 +1,11 @@
+package com.venom.synapse.domain.repo
+
+import com.venom.synapse.domain.model.QuestionProgressModel
+import kotlinx.coroutines.flow.Flow
+
+interface IProgressRepository {
+    fun observeProgress(questionId: Long): Flow<QuestionProgressModel?>
+    suspend fun saveProgress(progress: QuestionProgressModel)
+    suspend fun saveProgressBatch(list: List<QuestionProgressModel>)
+    suspend fun getProgress(questionId: Long): QuestionProgressModel?
+}
