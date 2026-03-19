4562e422537024fd59b694d23e257b95ec66dbcd
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-03-19 05:31:40 +0200

    add AppState and rememberSynapseAppState

---
+++ app/src/main/java/com/venom/synapse/navigation/AppState.kt
@@ -0,0 +1,30 @@
+package com.venom.synapse.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.venom.ui.navigation.BaseAppState
+
+class AppState(
+    navController: NavHostController,
+) : BaseAppState(navController) {
+
+    val currentScreen: SynapseScreen
+        get() = SynapseScreen.fromRoute(currentRoute)
+
+    val barConfig: BarConfig
+        get() = BarConfig.forRoute(currentRoute)
+}
+
+@Composable
+fun rememberSynapseAppState(
+    navController: NavHostController = rememberNavController(),
+): AppState {
+    val appState = remember(navController) { AppState(navController) }
+    val entry by navController.currentBackStackEntryAsState()
+    appState.updateCurrentEntry(entry)
+    return appState
+}
