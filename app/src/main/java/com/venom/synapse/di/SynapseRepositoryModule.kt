2d8fd44c243dafefa5199d8ad054431a69c0a5b7
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-03-22 22:45:35 +0200

    refactor: Update repository bindings and imports

--- app/src/main/java/com/venom/synapse/di/SynapseRepositoryModule.kt
+++ app/src/main/java/com/venom/synapse/di/SynapseRepositoryModule.kt
@@ -5,87 +5,62 @@ import com.venom.synapse.data.repo.AuthRepositoryImpl
 import com.venom.synapse.data.repo.LocalDataRepository
 import com.venom.synapse.data.repo.PackRepositoryImpl
 import com.venom.synapse.data.repo.PdfRepositoryImpl
+import com.venom.synapse.data.repo.PremiumRepositoryImpl
 import com.venom.synapse.data.repo.ProgressRepositoryImpl
 import com.venom.synapse.data.repo.QuestionRepositoryImpl
 import com.venom.synapse.data.repo.SessionRepositoryImpl
-import com.venom.synapse.data.repo.SupabasePremiumRepository
 import com.venom.synapse.data.repo.VisionRepositoryImpl
 import com.venom.synapse.domain.repo.IAIRepository
 import com.venom.synapse.domain.repo.IAuthRepository
 import com.venom.synapse.domain.repo.ILocalDataRepository
 import com.venom.synapse.domain.repo.IPackRepository
 import com.venom.synapse.domain.repo.IPdfRepository
+import com.venom.synapse.domain.repo.IPremiumRepository
 import com.venom.synapse.domain.repo.IProgressRepository
 import com.venom.synapse.domain.repo.IQuestionRepository
 import com.venom.synapse.domain.repo.ISessionRepository
+import com.venom.synapse.domain.repo.ISocialProofRepository
 import com.venom.synapse.domain.repo.IVisionRepository
-import com.venom.synapse.domain.repo.PremiumRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class SynapseRepositoryModule {
 
-    @Binds
-    @Singleton
-    abstract fun bindAuthRepository(
-        impl: AuthRepositoryImpl
-    ): IAuthRepository
+    @Binds @Singleton
+    abstract fun bindAuthRepository(impl: AuthRepositoryImpl): IAuthRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindPackRepository(
-        impl: PackRepositoryImpl
-    ): IPackRepository
+    @Binds @Singleton
+    abstract fun bindPackRepository(impl: PackRepositoryImpl): IPackRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindQuestionRepository(
-        impl: QuestionRepositoryImpl
-    ): IQuestionRepository
+    @Binds @Singleton
+    abstract fun bindQuestionRepository(impl: QuestionRepositoryImpl): IQuestionRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindProgressRepository(
-        impl: ProgressRepositoryImpl
-    ): IProgressRepository
+    @Binds @Singleton
+    abstract fun bindProgressRepository(impl: ProgressRepositoryImpl): IProgressRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindSessionRepository(
-        impl: SessionRepositoryImpl
-    ): ISessionRepository
+    @Binds @Singleton
+    abstract fun bindSessionRepository(impl: SessionRepositoryImpl): ISessionRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindAIRepository(
-        impl: AIRepositoryImpl
-    ): IAIRepository
+    @Binds @Singleton
+    abstract fun bindAIRepository(impl: AIRepositoryImpl): IAIRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindPdfRepository(
-        impl: PdfRepositoryImpl
-    ): IPdfRepository
+    @Binds @Singleton
+    abstract fun bindPdfRepository(impl: PdfRepositoryImpl): IPdfRepository
 
-    @Binds
-    @Singleton
+    @Binds @Singleton
     abstract fun bindVisionRepository(impl: VisionRepositoryImpl): IVisionRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindPremiumRepository(
-        impl: SupabasePremiumRepository
-    ): PremiumRepository
+    @Binds @Singleton
+    abstract fun bindPremiumRepository(impl: PremiumRepositoryImpl): IPremiumRepository
 
-    @Binds
-    @Singleton
-    abstract fun bindLocalDataRepository(
-        impl: LocalDataRepository,
-    ): ILocalDataRepository
+    @Binds @Singleton
+    abstract fun bindSocialProofRepository(impl: PremiumRepositoryImpl): ISocialProofRepository
+
+    @Binds @Singleton
+    abstract fun bindLocalDataRepository(impl: LocalDataRepository): ILocalDataRepository
 }
