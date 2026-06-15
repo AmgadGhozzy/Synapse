3018cfd20f84f85c64b61784bbce1b15af295245
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-06-15 14:39:31 +0300

    Remove residual files from legacy venom package

--- app/src/main/java/com/venom/synapse/features/premium/presentation/state/SocialProofData.kt
+++
@@ -1,9 +0,0 @@
-package com.venom.synapse.features.premium.presentation.state
-
-import androidx.compose.runtime.Immutable
-
-@Immutable
-data class SocialProofData(
-    val userCountLabel: String,
-    val avatarInitials: List<String> = listOf("A", "J", "M", "S", "K"),
-)
