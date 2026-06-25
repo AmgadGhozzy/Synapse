b2eb8942c47e3f32d3c6540c9cd1e02059d24244
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-06-25 13:15:42 +0300

    Add LocalSoundManager

---
+++ app/src/main/java/io/synapse/ai/core/audio/LocalSoundManager.kt
@@ -0,0 +1,6 @@
+package io.synapse.ai.core.audio
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import io.synapse.ai.core.audio.SoundManager
+
+val LocalSoundManager = staticCompositionLocalOf<SoundManager?> { null }
